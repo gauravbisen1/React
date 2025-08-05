86a9f11fdd4fb7b53ee9e0668df60079efccf26e
gauravbisen1 <gouravbisen123@gmail.com>
2025-08-05 19:05:39 +0530

    :(

--- Framer-motion/src/App.jsx
+++ Framer-motion/src/App.jsx
@@ -1,25 +1,94 @@
 import React from 'react'
-import {motion} from 'motion/react'
+import { motion,useScroll} from 'motion/react'
 
 const App = () => {
+  const scrollYprogress = useScroll().scrollYProgress
   return (
-    <div>
-      <motion.div
-       className='box'
-       animate={{
-        x:1200,
-        rotate:360
-       }}
-       transition={{
-        duration:3,
-        delay:1
-       }}
-      >
+    <div className='p-20 text-white text-center'>
 
-      </motion.div>
+        <motion.div 
+        style={{
+            scaleX:scrollYprogress
+        }}
+        className='bg-red-500 origin-left w-full h-3 fixed top-0 left-0'>
 
+        </motion.div>
+
+        <h2 className='text-4xl font-bold mb-8'>GAURAV BISEN</h2>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A magni consectetur tempora, facilis fugiat non obcaecati itaque cumque voluptatibus in dignissimos aspernatur quaerat inventore voluptatem recusandae, illo esse excepturi eligendi vitae nihil sint! Harum quo voluptas deleniti quisquam, id quos odio tenetur, mollitia dolorem temporibus expedita et repellendus optio voluptate molestiae natus saepe obcaecati nam a quibusdam illum laudantium cumque reiciendis quaerat? Ab suscipit nam illo expedita inventore laudantium culpa dignissimos repudiandae, neque,<br /><br /> illum aut autem molestiae magnam recusandae cupiditate! Iure error doloribus illum voluptatem minus qui fugit, deleniti totam, explicabo quia accusantium repellat, magnam modi voluptas officia quam? Omnis rem commodi corporis eveniet temporibus, totam quibusdam quae nisi quaerat, velit ab amet natus facilis eaque, in laboriosam ut excepturi provident veritatis. Velit cumque quisquam veritatis voluptatem illum error exercitationem veniam facere facilis dicta iure nostrum, dolore a non quae, doloribus quaerat labore beatae blanditiis suscipit odit. Commodi, mollitia dicta natus totam ipsam<br /><br /><br /> ipsum? Alias voluptate ea tenetur autem vitae cumque adipisci veniam, et voluptatem odit ipsa natus sequi quam rem fugiat, mollitia omnis tempore exercitationem consectetur, minus quia eaque quibusdam vel consequuntur? Odio amet reprehenderit laborum officia placeat! Mollitia voluptatibus unde aut reprehenderit eaque facere, cupiditate itaque inventore perspiciatis consequatur, odit sapiente! Fugiat, ad incidunt quia a repellat aspernatur? Corporis, deleniti perspiciatis deserunt sint vero laudantium eos quidem nemo error tenetur excepturi ipsum necessitatibus. Fuga delectus consectetur quos accusantium?<br /><br /><br /> Minima placeat numquam molestias, quos consequatur laboriosam. Culpa, sequi illum. Tempora animi commodi alias corrupti nisi, quas porro optio dicta? Libero doloremque quibusdam necessitatibus consequuntur placeat distinctio explicabo sunt, unde laudantium sapiente, asperiores adipisci expedita delectus non, possimus dolorem deleniti ut beatae? Sed, consequuntur consequatur iste tempora provident mollitia! Sit ipsam possimus qui nam error voluptatem mollitia, itaque, similique ea, temporibus minima molestiae aspernatur beatae placeat dolorum fugit sunt dolore unde tempora fugiat nostrum consectetur illo. Assumenda totam quod accusamus ipsa illo incidunt non temporibus vel, nihil accusantium? Ipsam provident tempore officiis recusandae<br /><br /><br /> aspernatur deserunt assumenda, earum incidunt nostrum ex eaque quis fuga qui amet voluptates tenetur reprehenderit nihil. Reiciendis necessitatibus molestias, odit explicabo nam quos dolore accusamus consequatur numquam expedita dolores impedit quisquam vel est! Quo doloremque illum veniam eos nisi vitae cupiditate asperiores velit earum! Tempore adipisci sit, nobis et dolores mollitia. Iusto earum ea odit error illo quae non. Doloremque doloribus porro et nesciunt minus dolorum, corporis, unde praesentium expedita nihil molestias quia quaerat itaque sequi rem repellendus inventore ipsum cumque eligendi illum labore! Eaque laborum sapiente nam nihil dolores excepturi, consequatur rem nostrum numquam exercitationem, doloribus ad, quisquam facere commodi delectus vero qui perspiciatis in fugiat neque ipsum recusandae quo quaerat. Excepturi tempore optio obcaecati error incidunt rem consectetur non, natus, id quaerat molestiae commodi dolorum dolore<br /><br /><br /> itaque perferendis at veniam tenetur debitis illum asperiores quos explicabo voluptatibus maxime repellat. Deserunt, distinctio? Repellendus distinctio laudantium voluptas quidem nesciunt rerum vitae pariatur id est nihil quaerat fuga quisquam vel deleniti dicta corporis quibusdam earum dolorum molestias amet, expedita impedit dolores consectetur cum. Ipsum sequi, inventore, mollitia repellendus tenetur quo minus alias recusandae nam est ducimus, distinctio voluptatibus. Dicta, deserunt. Qui voluptatem accusamus quia illo sequi ab? Fugiat, repudiandae minima. Ad ab necessitatibus ipsum dolor sit rerum dolorem incidunt maiores veniam fugit magnam distinctio, quibusdam ullam laudantium, assumenda voluptate dolore suscipit rem laborum temporibus? Est ad nostrum neque possimus corrupti nihil ullam, sunt eius odio non, ipsa aperiam id a optio ex libero pariatur blanditiis inventore eum aliquam minima vel animi. Expedita magni unde dolores quod aperiam porro ullam eaque facilis nam neque modi officia ea vero consectetur, doloremque voluptate sint ex.<br /><br /><br /> Voluptas culpa animi ab necessitatibus natus nisi ratione facere laboriosam cumque, illo adipisci perferendis voluptatum doloribus distinctio at placeat. Necessitatibus, voluptate aliquam eum quas quidem labore ad enim, nisi harum blanditiis dolore voluptatibus molestiae autem quo nulla, dignissimos tenetur quasi nesciunt et officia pariatur esse illo libero! Voluptas laudantium doloremque excepturi ratione autem, nisi, nulla veniam, consequatur quaerat deleniti laborum. Eaque quis iusto eum recusandae atque perspiciatis minima excepturi dolorum libero unde distinctio consequuntur ab, quo dolor cum illum, vitae aperiam consectetur iste. Doloremque excepturi nostrum unde dolor magni eligendi deleniti quasi eius amet culpa. Optio quam sapiente consequuntur itaque cum minus maiores? Reprehenderit repudiandae vero debitis velit porro quo sequi non aspernatur. Pariatur deserunt quam quasi tempore. Quas et omnis tempora ex impedit praesentium, exercitationem possimus nemo voluptatum illum soluta, aliquid deleniti blanditiis quidem ab quam, distinctio veritatis adipisci sequi sunt consequuntur consectetur at? Quibusdam nihil officia ab, reiciendis qui, voluptas similique neque quo adipisci hic fugit, mollitia tenetur? Ex maxime dolor fugit<br /><br /><br /> asperiores quo officiis corrupti tempora fuga assumenda hic consectetur provident architecto eos odit nobis illum soluta mollitia, quae reiciendis labore quia! Cum deserunt libero totam quaerat optio at expedita nihil, accusantium esse dicta doloremque natus, veniam soluta consequatur reprehenderit! Eos repellendus fuga, minima similique dicta praesentium odit laborum! Magnam, deleniti consequuntur! Explicabo veniam aperiam ipsa dolor laborum asperiores recusandae porro reprehenderit dolore ad fugit neque architecto fuga distinctio sit quae ex, possimus nisi. Ab illo expedita eligendi corrupti voluptate libero vitae? Alias quos doloribus voluptatem eveniet, expedita aspernatur quod facilis officia impedit beatae sunt architecto rerum dignissimos quam illum veniam consectetur minus sint repellendus delectus debitis qui accusamus. Qui voluptatum ab nesciunt excepturi doloremque sed quibusdam blanditiis debitis. Aspernatur mollitia aperiam doloremque, amet voluptatum magnam eius sed facilis vitae laborum error cumque, natus quisquam delectus animi itaque cum quo cupiditate explicabo praesentium, ipsum consequatur eum voluptates! Ducimus at vel facilis libero distinctio temporibus consequuntur nam adipisci quaerat, similique sit nisi veniam? Iusto distinctio corrupti aliquam. Modi illo accusamus nihil error quam ducimus aspernatur voluptates incidunt ipsam! Sint aliquid ipsa, perspiciatis culpa consequatur sit voluptatum atque natus aperiam officia quis aut hic inventore corporis eum reiciendis velit. Maxime, dolorum placeat! Possimus iste quos commodi odio placeat rem ipsa illo voluptas vero dignissimos unde excepturi hic necessitatibus, quibusdam eius modi nam, quo nostrum inventore. Veniam dolore dolor cum earum dolores sit. Quos numquam assumenda consequuntur magni molestias voluptates voluptas laboriosam minus quae blanditiis?</p>
+
+        <div className='bg-red-500 w-full h-3 absolute  left-0'></div>
     </div>
+
+    
   )
 }
 
 export default App
+
+
+//codes
+/* <motion.div
+       className='box'
+       initial={{
+        x:100
+       }}
+       animate={{
+        x:500,
+        rotate:360,
+       }}
+       transition={{
+        duration:2,
+        delay:1,
+        repeat:2, //infinite for contnous
+        ease:'anticipate'
+       }}
+      >
+
+      </motion.div> */
+
+
+{/* <motion.img 
+      animate={{
+        x:500,
+        rotate:360
+      }}
+      transition={{
+        duration:2,
+        delay:1,
+        repeat:1,
+        ease:'anticipate'
+      }}
+      src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b88f884e-d134-4606-a5ab-0eb9bcfaaa36/dg3ijoi-4b8672f1-e0df-401e-ab48-bcd417e1b993.png/v1/fit/w_828,h_1312/goku_ssj_god_png_format_by_gogitoytb_dg3ijoi-414w-2x.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MjAyNyIsInBhdGgiOiJcL2ZcL2I4OGY4ODRlLWQxMzQtNDYwNi1hNWFiLTBlYjliY2ZhYWEzNlwvZGczaWpvaS00Yjg2NzJmMS1lMGRmLTQwMWUtYWI0OC1iY2Q0MTdlMWI5OTMucG5nIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.60MwWcpJxI5KNw2UZUlW23pD2Tr2NoLlsCUHkDQZ6MQ" alt="" /> */}
+
+
+// keyframes
+// animate={{
+//   x:[0,600,600,0,0],
+//   y:[0,0,400,400,0],
+//   rotate:360
+// }}
+// transition={{
+//   duration:4,
+//   delay:1
+// }}
+
+
+// Drag
+// <motion.div 
+//         drag
+//         whileDrag={{
+//           scale:0.8
+//         }}
+//         dragConstraints={{
+//           left:0,
+//           right:1000,
+//           top:0,
+//           bottom:1000
+//         }}
+//         dragDirectionLock='true'
+//         className="box"
+//       >
+// GAURAV
+//       </motion.div>
