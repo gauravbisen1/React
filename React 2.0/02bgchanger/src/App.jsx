1472d7ea4526bf85523b86e33164d0cbb4f90da1
gauravbisen1 <gouravbisen123@gmail.com>
2025-09-18 00:19:43 +0530

    ai

--- React 2.0/02bgchanger/src/App.jsx
+++ React 2.0/02bgchanger/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import ai from "./assets/ai.png"
 
 function App() {
   const [color , setColor] = useState("black")
@@ -8,7 +9,9 @@ function App() {
 
   return (
     <div className=' w-full h-screen duration-200' style={{backgroundColor : color}}>
-      
+        <div className=' flex justify-center '>
+            <img className='w-50 h-50 mt-50' src={ai} alt="img" />
+        </div>
         <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
             <div className=' flex flex-wrap justify-center gap-3 shadow-lg bg-white rounded-3xl px-3 py-2'>
                 <button onClick={()=>setColor("red")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{color:"black",backgroundColor:"red"}}>Red</button>
